029dbf63822b037b7bdfdea010c4b6a478785c63
maker-lee <hare0529@naver.com>
2023-07-11 13:39:58 +0900

    지도 시각화 및 기술통계 연습문제

--- helper.py
+++ helper.py
@@ -43,6 +43,36 @@ def replaceMisttingValue(df) :
     imr = SimpleImputer(missing_values=np.nan,strategy='mean')
     
 
+def setCategory(df,ignore=[]) :
+    cdf = df.copy()
+    # 데이터 프레임의 변수명을 리스트로 변환
+    ilist = list(cdf.dtypes.index)
+    # 데이터 프레임의 변수형을 리스트로 변환
+    vlist = list(cdf.dtypes.values)
+
+    # 변수형에 대한 반복 처리
+    for i, v in enumerate(vlist):
+        # 변수형이 object이면?
+        if v == 'object':
+            # 변수명을 가져온다.
+            field_name = ilist[i]
+            # 가져온 변수명에 대해 값의 종류별로 빈도를 카운트 한 후 인덱스 이름순으로 정렬
+            vc = cdf[field_name].value_counts().sort_index()
+            #print(vc)
+
+            # 인덱스 이름순으로 정렬된 값의 종류별로 반복 처리
+            for ii, vv in enumerate(list(vc.index)):
+                # 일련번호값 생성
+                vnum = ii + 1
+                #print(vv, " -->", vnum)
+
+                # 일련번호값으로 치환
+                cdf.loc[cdf[field_name] == vv, field_name] = vnum
+
+            # 해당 변수의 데이터 타입을 범주형으로 변환
+            cdf[field_name] = cdf[field_name].astype('category')    
+    return cdf
+
 
 
 
