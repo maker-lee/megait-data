f09e43f3ecd56e62b68b4db1d54682c1e2a77aea
maker-lee <hare0529@naver.com>
2023-07-19 17:43:24 +0900

    두 변수간의 차이 분석 수정

--- helper.py
+++ helper.py
@@ -1,7 +1,12 @@
 # 사용되는 함수 모음 
 import numpy as np
-from pandas import DataFrame
+from pandas import DataFrame, MultiIndex, concat
+from math import sqrt
+from scipy.stats import t
 from sklearn.impute import SimpleImputer
+from scipy.stats import shapiro, normaltest, ks_2samp, bartlett, fligner, levene, chi2_contingency
+
+
 
 def getIq(field):
     q1 = field.quantile(q=0.25)
@@ -114,4 +119,89 @@ def get_confidence_interval(data, clevel=0.95):
     cmin, cmax = t.interval(clevel, dof, loc=sample_mean, scale=sample_std_error)
     
     return (cmin, cmax)
-    
+
+
+
+def normality_test(*any):
+    names = []
+
+    result = {
+        'statistic': [],
+        'p-value': [],
+        'result': []
+    }
+    for i in any:
+        s, p = shapiro(i)
+        result['statistic'].append(s)
+        result['p-value'].append(p)
+        result['result'].append(p > 0.05)
+        names.append(('정규성', 'shapiro', i.name))
+
+    for i in any:
+        s, p = normaltest(i)
+        result['statistic'].append(s)
+        result['p-value'].append(p)
+        result['result'].append(p > 0.05)
+        names.append(('정규성', 'normaltest', i.name))
+
+    n = len(any)
+
+    for i in range(0, n):
+        j = i + 1 if i < n - 1 else 0
+
+        s, p = ks_2samp(any[i], any[j])
+        result['statistic'].append(s)
+        result['p-value'].append(p)
+        result['result'].append(p > 0.05)
+        names.append(('정규성', 'ks_2samp', f'{any[i].name} vs {any[j].name}'))
+
+    return DataFrame(result, index=MultiIndex.from_tuples(names, names=['condition', 'test', 'field']))
+
+
+def equal_variance_test(*any):
+    # statistic=1.333315753388535, pvalue=0.2633161881599037
+    s1, p1 = bartlett(*any)
+    s2, p2 = fligner(*any)
+    s3, p3 = levene(*any)
+
+    names = []
+
+    for i in any:
+        names.append(i.name)
+
+    fix = " vs "
+    name = fix.join(names)
+    index = [['등분산성', 'Bartlett', name], ['등분산성', 'Fligner', name], ['등분산성', 'Levene', name]]
+
+    df = DataFrame({
+        'statistic': [s1, s2, s3],
+        'p-value': [p1, p2, p3],
+        'result': [p1 > 0.05, p2 > 0.05, p3 > 0.05]
+    }, index=MultiIndex.from_tuples(index, names=['condition', 'test', 'field']))
+
+    return df
+
+def independence_test(*any):
+    df = DataFrame(any).T
+    result = chi2_contingency(df)
+
+    names = []
+
+    for i in any:
+        names.append(i.name)
+
+    fix = " vs "
+    name = fix.join(names)
+
+    index = [['독립성', 'Chi2', name]]
+
+    df = DataFrame({
+        'statistic': [result.statistic],
+        'p-value': [result.pvalue],
+        'result': [result.pvalue > 0.05]
+    }, index=MultiIndex.from_tuples(index, names=['condition', 'test', 'field']))
+
+    return df
+
+def all_test(*any):
+    return concat([normality_test(*any), equal_variance_test(*any), independence_test(*any)])
